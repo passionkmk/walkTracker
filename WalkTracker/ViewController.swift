4f426ba06d92f4ce3cacabb403d9754c924dfa71
passionkmk <passion.kmk@gmail.com>
2019-01-19 15:29:47 +0900

    Initial Commit

---
+++ WalkTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WalkTracker
+//
+//  Created by 김민구 on 19/01/2019.
+//  Copyright © 2019 김민구. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WalkTrackerTests/WalkTrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  WalkTrackerTests.swift
+//  WalkTrackerTests
+//
+//  Created by 김민구 on 19/01/2019.
+//  Copyright © 2019 김민구. All rights reserved.
+//
+
+import XCTest
+@testable import WalkTracker
+
+class WalkTrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
