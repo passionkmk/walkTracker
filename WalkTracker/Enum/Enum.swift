3a9321192933c4fd0371b25e45a3851ab341bc37
passionkmk <passion.kmk@gmail.com>
2019-01-23 23:58:07 +0900

    Add Enum, ViewController Style for UI

---
+++ WalkTracker/Enum/Enum.swift
@@ -0,0 +1,15 @@
+//
+//  Enum.swift
+//  WalkTracker
+//
+//  Created by 김민구 on 23/01/2019.
+//  Copyright © 2019 김민구. All rights reserved.
+//
+
+import Foundation
+
+enum WalkStatus {
+    case ready
+    case walking
+    case finish
+}
---
+++ WalkTracker/ViewController/WalkViewController+Style.swift
@@ -0,0 +1,25 @@
+//
+//  WalkViewController+Style.swift
+//  WalkTracker
+//
+//  Created by 김민구 on 20/01/2019.
+//  Copyright © 2019 김민구. All rights reserved.
+//
+
+import UIKit
+
+extension WalkViewController {
+    enum Ready {
+        
+    }
+    
+    enum Walking {
+        
+    }
+    
+    enum Stop {
+        
+    }
+}
+
+
